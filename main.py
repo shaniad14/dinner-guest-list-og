a986a261922c7c72673299fdcac2cdc39358e3f6
shaniad14 <shaniad1414@gmail.com>
2026-04-06 14:20:53 -0700

    Did the code for adding a guest.

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ def get_name():
         if name:
             return name
         print("Name cannot be empty.")
+
+def add_guest():
+    name = get_name()
+    if name in guests:
+        print("Name already exists.")
+    else:
+        guests.append(name)
+        print(name, "added.")
